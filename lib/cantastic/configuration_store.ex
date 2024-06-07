b55ddf1c40721f379bf8ce5867a435d7678c6d02
Thibault Poncelet <thibault@spin42.com>
2024-06-07 11:58:01 +0200

    allow to split config files

--- lib/cantastic/configuration_store.ex
+++ lib/cantastic/configuration_store.ex
@@ -49,14 +49,40 @@ defmodule Cantastic.ConfigurationStore do
     opt_app              = Application.get_env(:cantastic, :otp_app)
     priv_can_config_path = Application.get_env(:cantastic, :priv_can_config_path)
     config_path          = Path.join(:code.priv_dir(opt_app), priv_can_config_path)
-    with {:ok, config}  <- YamlElixir.read_from_file(config_path, atoms: true),
-         {:ok, encoded} <- Jason.encode(config)
+    read_yaml(config_path)
+  end
+
+  defp read_yaml(path) do
+    with {:ok, config}  <- YamlElixir.read_from_file(path, atoms: true),
+         {:ok, encoded} <- Jason.encode(config),
+         {:ok, decoded} <- encoded |> Jason.decode(keys: :atoms)
     do
-      encoded |> Jason.decode(keys: :atoms)
+      base_path = path |> Path.dirname()
+      interpret_node(base_path, decoded)
     else
       {:error, error} -> {:error, error}
     end
   end
+
+  defp interpret_node(base_path, node) when is_map(node) do
+    interpreted_node = node |> Enum.reduce(%{}, fn({key, value}, acc) ->
+      {:ok, interpreted_value} = interpret_node(base_path, value)
+      acc |> Map.put(key, interpreted_value)
+    end)
+    {:ok, interpreted_node}
+  end
+  defp interpret_node(base_path, node) when is_list(node) do
+    interpreted_node = node |> Enum.map(fn(value) ->
+      {:ok, interpreted_value} = interpret_node(base_path, value)
+      interpreted_value
+    end)
+    {:ok, interpreted_node}
+  end
+  defp interpret_node(base_path, "import!:" <> path) do
+    full_path   = Path.join([base_path, path])
+    read_yaml(full_path)
+  end
+  defp interpret_node(_, node), do: {:ok, node}
 
   defp compute_networks() do
     raw_can_network_specifications = (Application.get_env(:cantastic, :can_networks) || "") |> String.split(",", trim: true)
